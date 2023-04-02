cf83d3920695fc38296e4df7ff89db5fccce3426
PashaAllahverdiyev <pashama@code.edu.az>
2023-04-02 20:55:42 +0400

    code.cs

---
+++ code.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = { 3, 4, -2, 1, -5, 6 };
+int count = 0;int count2 = 0;
+for (int i = 0; i < numbers.Length;i++)
+{ 
+    if (numbers[i] > 0) 
+    {
+        count++;
+    }else
+    {
+        count2++;
+    }
+    
+        
+   
+}
+Console.WriteLine(count);
+Console.WriteLine(count2);
